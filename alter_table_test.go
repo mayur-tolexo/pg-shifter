e1e4b1c95db971d48ad4a02c230358da338e541c
Mayur :) <md@pooraa.com>
2019-12-13 17:28:39 +0530

    modify to all pg type test added

--- alter_table_test.go
+++ alter_table_test.go
@@ -24,11 +24,110 @@ type TestTable struct {
 	City      string   `json:"city" sql:"city,type:text"`
 }
 
+func TestAlterColumnTypeModify(t *testing.T) {
+
+	if conn, err := psql.Conn(true); err == nil {
+		s := NewShifter()
+		assert := assert.New(t)
+		err = s.CreateTable(conn, &TestTable{})
+		for _, curStruct := range getAllTypeStruct() {
+			err = s.AlterTable(conn, curStruct, true)
+			assert.NoError(err)
+		}
+		err = s.DropTable(conn, &TestTable{}, true)
+		assert.NoError(err)
+	}
+}
+
 func getAllTypeStruct() []interface{} {
 	diffStruct := []interface{}{
 		&struct {
 			tableName struct{} `sql:"test_table"`
-			City      string   `json:"city" sql:"city,type:line"`
+			City      string   `json:"city" sql:"city,type:xml"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:uuid"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:txid_snapshot"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:tsvector"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:tsquery"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:timestamptz"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:timestamp"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:timetz"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:time"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:text"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:smallint"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:real"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:polygon"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:polygon"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:point"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:pg_lsn"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:path"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:numeric"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:money"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:macaddr8"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:macaddr"`
+		}{},
+		&struct {
+			tableName struct{} `sql:"test_table"`
+			City      string   `json:"city" sql:"city,type:lseg"`
 		}{},
 		&struct {
 			tableName struct{} `sql:"test_table"`
@@ -104,19 +203,4 @@ func getAllTypeStruct() []interface{} {
 		}{},
 	}
 	return diffStruct
-}
-
-func TestAlterColumnTypeModify(t *testing.T) {
-
-	if conn, err := psql.Conn(true); err == nil {
-		s := NewShifter()
-		assert := assert.New(t)
-		err = s.CreateTable(conn, &TestTable{})
-		for _, curStruct := range getAllTypeStruct() {
-			err = s.AlterTable(conn, curStruct, true)
-			assert.NoError(err)
-		}
-		err = s.DropTable(conn, &TestTable{}, true)
-		assert.NoError(err)
-	}
 }
